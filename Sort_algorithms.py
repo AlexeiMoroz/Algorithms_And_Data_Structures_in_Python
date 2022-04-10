7a3fa239336e1d53a1e23b6f5b34c50f68c6e656
geekbrains <clareon@gmail.com>
2022-04-10 16:55:26 +0300

    quick_sort

--- Sort_algorithms.py
+++ Sort_algorithms.py
@@ -35,7 +35,7 @@ def shell_sot(array):
     assert len(array)<4000,  'Only array before 200 numbers are allowed'
     def new_increment_array(array):
         inc = [1,4,10,23,57,132,301,701,1750]
-        #print(inc[-1])
+
 
         while len(array)<inc[-1]:
             #print(inc[-1])
@@ -51,15 +51,49 @@ def shell_sot(array):
                     break
                 array[j], array[j-increment] = array[j-increment], array[j]
 
+def quick_sort(array):
+    if len(array) <= 1:
+        return array
+    pivot = random.choice(array)
+    s_arr = []
+    m_arr = []
+    l_arr = []
+    for item in array:
+        if item < pivot:
+            s_arr.append(item)
+        elif item > pivot:
+            l_arr.append(item)
+        elif item == pivot:
+            m_arr.append(item)
+        else:
+            raise Exception('Unknown situation')
+    return quick_sort(s_arr)+m_arr+quick_sort(l_arr)
 
+def quick_sort_no_memory(array,frst,lst):
+    if frst >= lst:
+        return
+    pivot = array[random.randint(frst,lst)]
+    i,j = frst,lst
+    while i <= j:
+        while array[i]<pivot:
+            i+=1
+        while array[j]>pivot:
+            j-=1
+        if i <= j:
+            array[i],array[j] = array[j], array[i]
+            i,j = i+1,j-1
+    quick_sort_no_memory(array, frst, j)
+    quick_sort_no_memory(array, i, lst)
 
-
-array = [i for i in range(-100, 100)]
+array = [i for i in range(-10, 10)]
 print(array)
 random.shuffle(array)
 print(array)
 #bubble_sort(array)
 #selection_sort(array)
 #insertion_sort(array)
-shell_sot(array)
+#shell_sot(array)
+#arr_new = quick_sort(array)
+#print(arr_new)
+quick_sort_no_memory(array,0,len(array)-1)
 print(array)
