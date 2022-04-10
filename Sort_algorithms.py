98e0416026b82c238cec46a7284f396dd4e83a31
geekbrains <clareon@gmail.com>
2022-04-10 15:55:47 +0300

    shell-sort

--- Sort_algorithms.py
+++ Sort_algorithms.py
@@ -3,6 +3,7 @@
 import random
 
 def bubble_sort(array):
+
     n = 1
     while n < len(array):
         for i in range(len(array)-n):
@@ -11,6 +12,7 @@ def bubble_sort(array):
         n+=1
 
 def selection_sort(array):
+
     for i in range(len(array)):
         idx_min = i
         for j in range(i+1, len(array)):
@@ -19,6 +21,7 @@ def selection_sort(array):
         array[i],array[idx_min] = array[idx_min],array[i]
 
 def insertion_sort(array):
+
     for i in range(1, len(array)):
         spam = array[i]
         j = i
@@ -27,6 +30,29 @@ def insertion_sort(array):
             j-=1
         array[j] = spam
 
+def shell_sot(array):
+
+    assert len(array)<4000,  'Only array before 200 numbers are allowed'
+    def new_increment_array(array):
+        inc = [1,4,10,23,57,132,301,701,1750]
+        #print(inc[-1])
+
+        while len(array)<inc[-1]:
+            #print(inc[-1])
+            inc.pop()
+
+        while len(inc)>0:
+            yield inc.pop()
+
+    for increment in new_increment_array(array):
+        for i in range(increment, len(array)):
+            for j in range(i, increment - 1, -increment):
+                if array[j-increment] <= array[i]:
+                    break
+                array[j], array[j-increment] = array[j-increment], array[j]
+
+
+
 
 array = [i for i in range(-100, 100)]
 print(array)
@@ -34,5 +60,6 @@ random.shuffle(array)
 print(array)
 #bubble_sort(array)
 #selection_sort(array)
-insertion_sort(array)
+#insertion_sort(array)
+shell_sot(array)
 print(array)
