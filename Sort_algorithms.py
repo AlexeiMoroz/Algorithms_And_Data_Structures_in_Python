1b9b52a07a9bfc1cfe29d16f9455334d0f4781f9
geekbrains <clareon@gmail.com>
2022-04-10 14:40:55 +0300

    selection sort

--- Sort_algorithms.py
+++ Sort_algorithms.py
@@ -6,7 +6,7 @@ def bubble_sort(array):
     n = 1
     while n < len(array):
         for i in range(len(array)-n):
-            if array[i]>array[i+1]:
+            if array[i]<array[i+1]:
                 array[i], array[i+1] =  array[i+1],array[i]
         n+=1
 
@@ -18,11 +18,21 @@ def selection_sort(array):
                 idx_min = j
         array[i],array[idx_min] = array[idx_min],array[i]
 
+def insertion_sort(array):
+    for i in range(1, len(array)):
+        spam = array[i]
+        j = i
+        while array[j-1] > spam and j>0:
+            array[j] = array[j - 1]
+            j-=1
+        array[j] = spam
+
 
 array = [i for i in range(-100, 100)]
 print(array)
 random.shuffle(array)
 print(array)
 #bubble_sort(array)
-selection_sort(array)
+#selection_sort(array)
+insertion_sort(array)
 print(array)
