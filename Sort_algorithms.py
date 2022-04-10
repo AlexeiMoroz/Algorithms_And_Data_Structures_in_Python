b7f0dfbb7b8e77d7ab46c6292b167dcb07295561
geekbrains <clareon@gmail.com>
2022-04-10 14:14:08 +0300

    select algo added

--- Sort_algorithms.py
+++ Sort_algorithms.py
@@ -1,4 +1,4 @@
-#Bubble sort and other sort algorithms
+#Bubble sort
 
 import random
 
@@ -10,11 +10,19 @@ def bubble_sort(array):
                 array[i], array[i+1] =  array[i+1],array[i]
         n+=1
 
+def selection_sort(array):
+    for i in range(len(array)):
+        idx_min = i
+        for j in range(i+1, len(array)):
+            if (array[idx_min]>array[j]):
+                idx_min = j
+        array[i],array[idx_min] = array[idx_min],array[i]
 
 
 array = [i for i in range(-100, 100)]
 print(array)
 random.shuffle(array)
 print(array)
-bubble_sort(array)
+#bubble_sort(array)
+selection_sort(array)
 print(array)
